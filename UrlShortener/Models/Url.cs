fd3c9967a4672aeb7690ef6a3cb789c049cbd1f8
YunusAlamm <alamdari.yoonos@gmail.com>
2024-05-13 12:19:34 +0330

    Define the model

---
+++ UrlShortener/Models/Url.cs
@@ -0,0 +1,10 @@
+namespace Models.Url
+{
+    public  class Url
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string ShortenedUrl { get; set; }
+    }
+    
+}
