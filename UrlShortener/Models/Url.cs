0b67cff74371fd530008c65766f6448d1c845201
YunusAlamm <alamdari.yoonos@gmail.com>
2024-05-25 11:29:52 +0330

    Update Url.cs

--- UrlShortener/Models/Url.cs
+++ UrlShortener/Models/Url.cs
@@ -1,11 +1,14 @@
-namespace Models.Url
+namespace UrlShortener.Models
 {
-    public  class Url
+    public class Url
     {
         public int Id { get; set; }
-        
-        public string OriginalUrl { get; set; }
-        public string ShortenedUrl { get; set; }
+
+        public string OriginalUrl { get; set; } =string.Empty;
+        public string ShortenedUrl { get; set; } =string.Empty;
+        public string Alias { get; set; } =string.Empty;
+        public int Counter { get; set; } = 0;
+        public User User { get; set; }
     }
-    
+
 }
