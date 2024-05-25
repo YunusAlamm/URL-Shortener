d6feab9afcd104e4bf22cbc61d6ed00203ed47bd
YunusAlamm <alamdari.yoonos@gmail.com>
2024-05-25 11:27:24 +0330

    Add Login & Register Actions

--- UrlShortener/Controllers/UrlController.cs
+++ UrlShortener/Controllers/UrlController.cs
@@ -1,22 +1,82 @@
 using Microsoft.AspNetCore.Mvc;
-using Data.UrlContext;
-using Models.Url;
+using UrlShortener.Data;
 using Microsoft.EntityFrameworkCore;
+using UrlShortener.Models;
+using System.Security.Cryptography;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+using Microsoft.AspNetCore.Authorization;
+
+using Microsoft.Extensions.Options;
 
 namespace Controllers.UrlController
 {
-    
-    [Route("api/[controller]")]
+
+    [Route("[controller]")]
     [ApiController]
     public class UrlController : ControllerBase
     {
         private readonly UrlContext _context;
-        public UrlController(UrlContext context)
+        private readonly JwtSettings _jwtSettings;
+        public UrlController(UrlContext context ,IOptions<JwtSettings> jwtSettings)
         {
             _context = context;
+            _jwtSettings = jwtSettings.Value;
         }
 
-        [HttpPost]
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register(string UserName,string UserPassword)
+        {
+            if ( UserName == null || UserPassword == null)
+                return BadRequest("Fill the Name & password to sign-up");
+            
+
+            User user = new User();
+            var salt = GenerateSalt();
+            var hashedPassword = HashPasswordWithSalt(UserPassword, salt);
+            user.UserPassword = Convert.ToBase64String(hashedPassword);
+            user.Salt = Convert.ToBase64String(salt);
+            user.UserName = UserName;
+
+
+
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("User Registered!");    
+
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(String UserName, String UserPassword)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == UserName);
+            if (user == null)
+                return BadRequest("User Not found!");
+            
+            var ConfirmSubject = HashPasswordWithSalt(UserPassword,Encoding.UTF8.GetBytes(user.Salt));
+            var flag =  Convert.ToBase64String(ConfirmSubject);
+            if (user.UserPassword ==flag)
+            {
+                var token = GenerateJwtToken(user);
+
+                return Ok(new{Token = token});
+            }
+
+            return Unauthorized("ok nistim");
+
+        }
+       
+
+
+
+
+        [HttpPost("Create")]
+        [Authorize]
         public async  Task<IActionResult> Create(string OriginalUrl)
         {
             var q = await _context.Urls.FirstOrDefaultAsync(i => i.OriginalUrl == OriginalUrl);
@@ -26,7 +86,12 @@ namespace Controllers.UrlController
             url.OriginalUrl = OriginalUrl;
             url.ShortenedUrl = ShortUrlGenerator(url.OriginalUrl);
 
-           
+
+           var userName = User.Identity.Name;
+           var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+           user.Urls.Add(url);
+           url.User = user;
+
             _context.Add(url);
            await _context.SaveChangesAsync();
             
@@ -45,25 +110,35 @@ namespace Controllers.UrlController
     
     
     [HttpGet]
-    public List<Url> ShowAll(){
-        return _context.Urls.ToList();
+    [Authorize]
+    public async Task<IActionResult> ShowAll(){
+
+        var userName = User.Identity.Name;
+        var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName ==userName);
+
+        return (IActionResult)user.Urls.ToList();
+       
+
+        
     }
 
     [HttpDelete]
+    [Authorize]
     public  ActionResult Delete(){
         _context.Database.ExecuteSqlRaw("TRUNCATE TABLE Urls");
         
         return Ok("DataBase Cleared!");
     }
 
-    [HttpGet("/s/{ShortenedUrl}")]
-    private async Task<IActionResult> RedirectAddress(string ShortenedUrl){
+    [HttpGet("{RedirectAddress}")]
+    [Authorize]
+    public async Task<IActionResult> RedirectAddress(string ShortenedUrl){
 
         var q = await _context.Urls.FirstOrDefaultAsync(s => s.ShortenedUrl == ShortenedUrl);
 
         
 
-        return Redirect(q.ShortenedUrl);
+        return Redirect(q.OriginalUrl);
     }
 
 
@@ -75,7 +150,47 @@ namespace Controllers.UrlController
         }
 
 
-
+        private static byte[] GenerateSalt()
+{
+    using (var rng = new RNGCryptoServiceProvider())
+    {
+        var randomNumber = new byte[128 / 8]; // Generate a 128-bit salt
+        rng.GetBytes(randomNumber);
+        return randomNumber;
     }
+}
+
+    private static byte[] HashPasswordWithSalt(string password, byte[] salt)
+{
+    var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000); // Use PBKDF2 with 10000 iterations
+    return pbkdf2.GetBytes(256 / 8); // Generate a 256-bit hash
+}
+
+ 
+    private string GenerateJwtToken(User user)
+{
+    var tokenHandler = new JwtSecurityTokenHandler();
+    var key = Encoding.ASCII.GetBytes(_jwtSettings.Key);
+    var tokenDescriptor = new SecurityTokenDescriptor
+    {
+        Subject = new ClaimsIdentity(new[]
+        {
+            new Claim("Id", user.UserId),
+            new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        }),
+        Expires = DateTime.UtcNow.AddMinutes(5),
+        Issuer = _jwtSettings.Issuer,
+        Audience = _jwtSettings.Audience,
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+    };
+    var token = tokenHandler.CreateToken(tokenDescriptor);
+    return tokenHandler.WriteToken(token);
+}
+
+
+
+
     
+    }
 }
