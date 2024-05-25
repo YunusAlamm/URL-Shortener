6754442ed228e9d0eab737d36cd33c87e148e615
YunusAlamm <alamdari.yoonos@gmail.com>
2024-05-25 11:30:31 +0330

    move to postgresql and adding JWT configs

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,35 +1,69 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Data.UrlContext;
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using UrlShortener.Data;
+using UrlShortener.Models;
+// public class Program
+// {
+//     private static void Main(string[] args)
+//     {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+        builder.Services.AddControllers();
 
-builder.Services.AddDbContext<UrlContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionString")));
+        builder.Services.AddDbContext<UrlContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("UrlShortener")));
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddAuthentication(Options =>{
+            Options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            Options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            Options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(o =>
+        {
+            o.TokenValidationParameters = new TokenValidationParameters{
+               
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey
+                (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = false,
+                ValidateIssuerSigningKey = true
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    
-    app.UseSwaggerUI();
-}
+            };
+        }
+        );
+        builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
 
 
-app.UseHttpsRedirection();
+        builder.Services.AddAuthorization();
 
-app.UseAuthorization();
+        var app = builder.Build();
 
-app.MapControllers();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI();
+        }
+
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+        app.UseAuthentication();
+
+        app.MapControllers();
 
 
 
 
-app.Run();
-
-
+        app.Run();
+//     }
+// }
