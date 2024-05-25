dd603c52dacd26c71c6d9318f8735cba50b238bf
YunusAlamm <alamdari.yoonos@gmail.com>
2024-05-25 11:27:42 +0330

    update dbcontext

--- UrlShortener/Data/UrlContext.cs
+++ UrlShortener/Data/UrlContext.cs
@@ -1,16 +1,15 @@
 using Microsoft.EntityFrameworkCore;
-using Models.Url;
-
-namespace Data.UrlContext
+using UrlShortener.Models;
+namespace UrlShortener.Data
 {
-    public class UrlContext: DbContext
+
+    public class UrlContext:DbContext
     {
         public UrlContext(DbContextOptions<UrlContext> options): base(options)
-        {
+        {}
 
-        }
-
-        public DbSet<Url> Urls{ get; set; }
+        public DbSet<User> Users { get;set; }
+        public DbSet<Url> Urls { get;}
     }
-    
+
 }
