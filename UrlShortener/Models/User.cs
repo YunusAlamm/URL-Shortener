c7c4bfa11b53557231a9ec968e4fd21cf1f77af6
YunusAlamm <alamdari.yoonos@gmail.com>
2024-05-27 17:14:28 +0330

    Update User.cs

--- UrlShortener/Models/User.cs
+++ UrlShortener/Models/User.cs
@@ -4,13 +4,12 @@ namespace UrlShortener.Models
 {
     public class User
     {
-        private List<Url> urls;
-
-        public string UserId { get; set;}=string.Empty;
+      
+        public int Id { get; set;}
         public string UserName { get; set;}=string.Empty;
         public string UserPassword { get; set;}=string.Empty;
         public string Salt { get; set; } =string.Empty;   //needed for hashing the password.
-        public List<Url> Urls { get => urls; set => urls = value; }
+        public List<Url> Urls { get; set; } = new List<Url>();
 
 
 
